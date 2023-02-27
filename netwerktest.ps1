a3a271fc36c35d78bc9923fa98dc14e38297acd7
Optimize <61429602+PietjePuh@users.noreply.github.com>
2023-02-27 22:56:48 +0100

    Create netwerktest.ps1

---
+++ netwerktest.ps1
@@ -0,0 +1,15 @@
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$output_file = "$env:TEMP\info_output.txt"
+
+"Current time: $timestamp" | Out-File $output_file
+"Computer name: $env:COMPUTERNAME" | Out-File $output_file -Append
+"User name: $env:USERNAME" | Out-File $output_file -Append
+"Public IP address: $(Invoke-WebRequest -Uri 'https://api.ipify.org' -UseBasicParsing).Content" | Out-File $output_file -Append
+ipconfig /all | Out-File $output_file -Append
+route print | Out-File $output_file -Append
+tracert 8.8.8.8 | Out-File $output_file -Append
+nslookup google.com | Out-File $output_file -Append
+netsh lan show interfaces | Out-File $output_file -Append
+netsh wlan show all | Out-File $output_file -Append
+"=== Done gathering system information ===" | Out-File $output_file -Append
+notepad $output_file
